1e381c12fbfcb0328d2f8db66b1d6b1e4b2b0c39
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-23 03:59:30 +0330

    Done in pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider, Container } from "@mui/material";
+import { theme } from "@/theme";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container
+        // maxWidth="xl"
+        sx={{
+          w: "100%",
+        }}
+      >
+        <Component {...pageProps} />
+      </Container>
+    </ThemeProvider>
+  );
 }
